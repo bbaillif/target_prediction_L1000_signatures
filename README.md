920cc5073cb4af8704138b7d728cf0a152292e04
Benoît BAILLIF <benoit.baillif@gmail.com>
2021-02-26 12:44:44 +0100

    Added processing and first analysis files

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ The goal of this code is to preprocess data coming from the LINCS (CMap/L1000) a
  * [GSE70138](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE70138)
  * [GSE92742](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE92742)
 * [Pubchem](https://pubchem.ncbi.nlm.nih.gov/) using an available [Bioassay SQLite extract](http://biocluster.ucr.edu/~tbackman/bioassayR/pubchem_protein_only.sqlite) along with corresponding [R package](http://bioconductor.org/packages/release/bioc/html/bioassayR.html) for data extraction 
-* [LINCS data portal](http://lincsportal.ccs.miami.edu/dcic-portal/): to find additional Pubchem CID of profiled compounds ; used links are "outdated" and cannot be found currently please
+* [LINCS data portal](http://lincsportal.ccs.miami.edu/dcic-portal/): to find additional Pubchem CID of profiled compounds ; used links are "outdated" and cannot be found currently
 * [Broad Institute Drug Repurposing Hub](https://clue.io/repurposing): to find TUBB active compounds that are not in Pubchem
 
 # Script order
@@ -28,5 +28,22 @@ Scripts were written using Jupyter Notebook from conda 4.8.3, with Python 3.7.6
 * perturbagen_and_related_signatures_metadata_processing.ipynb
  Compile the 2 GSE metadata
  Select compound perturbagens
+ Find used compounds, meaning compounds having a 10 µM and 24 h signature in the 8 chosen core cell lines
+* pubchem_cid_extraction
+ Find all available Pubchem CID for used compounds in the analysis
+* target_data_processing
+ Produce the final activity matrix to be used downstream
 * pubchem_bioactivity_matrix_extraction.R
  Compute the bioactivity matrix using the bioassayR package along with the pubchem protein only SQLite file
+* signature_extraction.ipynb
+ Extract signatures of used compounds from the gctx archives
+* morgan_fingerprints_and_signatures_tsne
+ Compute t-SNE embeddings for used compounds and signature, to later plot the chemical and biological spaces
+* produce_space_plots
+ Produce figures corresponding to chemical and biological space plots
+
+* TODO models
+Compute random forest models, store performances in csv files
+
+* TODO distance plots
+Produce quadrant plots and statistics for the modeled targets
--- pubchem_bioactivity_matrix_extraction.R
+++ pubchem_bioactivity_matrix_extraction.R
@@ -2,12 +2,42 @@
 
 # Purpose: Extract Pubchem bioassay data using the rpubchem bioassayR package
 
-# Currently not possible because the used SQLite data are no more available, URL link is broken
 # https://cran.r-project.org/web/packages/rpubchem/rpubchem.pdf
 # http://bioconductor.org/packages/devel/bioc/vignettes/bioassayR/inst/doc/bioassayR.html
 # http://biocluster.ucr.edu/~tbackman/bioassayR/pubchem_protein_only.sqlite
 
-# Input : cmp_info_cmap to get compound info, mostly the InchiKey to catch CIDs
-# Pubchem protein only SQLite ; contains bioactivity data to be extracted with the bioassayR package
+# Input: pert_id_to_pubchem_cid.csv to find available cids for used compounds
+# pubchem_protein_only.sqlite contains bioactivity data to be extracted with the bioassayR package
 
-# Output : finalMat : 2D matrix (target * cid), containing also metadata on the targets (external name...)
+# Output: pubchem_activity_matrix.csv
+
+library(bioassayR)
+library(biomaRt)
+
+setwd("D:/Ben/Documents/science/Machine learning Bayer/paper_code")
+
+
+#target_final_mat = read.csv("../code/finalMat.csv")
+
+pertid_cid_map = read.csv("data/processed/pert_id_to_pubchem_cid.csv")
+cids = unique(pertid_cid_map$pubchem_cid)
+
+bioassayDB <- connectBioassayDB("data/raw/pubchem_protein_only.sqlite")
+selectedAssayData <- getBioassaySetByCids(bioassayDB, cids)
+selectedAssayData
+myActivityMatrix <- perTargetMatrix(selectedAssayData)
+
+uniprotTargets = lapply(targets, translateTargetId, database=bioassayDB, category="UniProt")
+uniprotTargets = sapply(uniprotTargets, function(x) x[1])
+
+ensembl <- useEnsembl(biomart="ensembl",dataset="hsapiens_gene_ensembl")
+proteinDetails <- getBM(attributes=c("description","uniprotswissprot","external_gene_name"), filters=c("uniprotswissprot"), mart=ensembl, values=uniprotTargets)
+proteinDetails <- proteinDetails[match(uniprotTargets, proteinDetails$uniprotswissprot),]
+
+finalMatrix <- myActivityMatrix[! is.na(proteinDetails[,1]),]
+proteinDetails <- proteinDetails[!is.na(proteinDetails[,1]),]
+finalMatrix = cbind(proteinDetails, as.data.frame(as.matrix(finalMatrix)))
+
+write.csv(finalMatrix, "data/processed/pubchem_bioactivity_matrix_2020.csv")
+
+# Output : pubchem_bioactivity : 2D matrix (target * cid), containing also metadata on the targets (external name...)
